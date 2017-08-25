3cebca192edc34836f413e56eb35290679d4bdb6
William Lemus <william.lemus@gmail.com>
2017-08-25 14:15:38 -0400

    Update login page styling and guest login functionality when logging in from sign up page. Refactor reducer structure in preperation for future reducers.

--- frontend/reducers/session/session_reducer.js
+++ frontend/reducers/session/session_reducer.js
@@ -1,4 +1,4 @@
-import {RECEIVE_ERRORS, RECEIVE_CURRENT_USER} from '../actions/session_actions';
+import {RECEIVE_ERRORS, RECEIVE_CURRENT_USER} from '../../actions/session_actions';
 
 const nullUser = {
   currentUser: null,
@@ -9,7 +9,7 @@ export const SessionReducer = (state = nullUser, action) =>{
   Object.freeze(state);
   switch(action.type){
     case RECEIVE_ERRORS:
-      return Object.assign({}, state, {errors: [action.errors]});
+      return Object.assign({}, state, {errors: action.errors});
     case RECEIVE_CURRENT_USER:
       return {currentUser: action.currentUser, errors: []};
     default:
