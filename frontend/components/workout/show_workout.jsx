b380d14a7d5ff92e5647c9c24f176aad786c0b14
William Lemus <william.lemus@gmail.com>
2017-08-26 16:49:19 -0400

    Show workouts page is now displaying workout data with map.

--- frontend/components/workout/show_workout.jsx
+++ frontend/components/workout/show_workout.jsx
@@ -2,9 +2,41 @@ import React from 'react';
 
 class ShowWorkout extends React.Component{
 
-  componentDidMount(){
-    this.props.getWorkout(this.props.match.params.id);
+
+  componentWillReceiveProps(newProps){
+    if(this.props.match.url !== newProps.match.url){
+      this.props.getWorkout(newProps.match.params.id)
+      .then(
+        () => this.initDrawing()
+      );
+    }
   }
+
+  componentDidMount(){
+    this.props.getWorkout(this.props.match.params.id)
+    .then(
+      () => this.initDrawing()
+    );
+
+  }
+
+  initDrawing(){
+    let workoutPolyline = new google.maps.Polyline({
+      path: google.maps.geometry.encoding.decodePath(this.props.workout.polyline),
+    });
+    let middle = Math.floor(workoutPolyline.latLngs.b[0].length /2);
+    let middlePoint =  workoutPolyline.latLngs.b[0].b[middle].toJSON();
+    const mapOptions = {
+      center: middlePoint,
+      zoom: 13,
+      disableDefaultUI: true,
+      zoomControl: true
+    };
+
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+    workoutPolyline.setMap(this.map);
+  }
+
 
   render(){
     if(this.props.workout){
@@ -22,7 +54,8 @@ class ShowWorkout extends React.Component{
             <li>{workout.type}</li>
             <li>{workout.start_datetime}</li>
           </ul>
-
+          <div id='map-container' ref={ map => this.mapNode = map}>
+          </div>
         </section>
       );
     } else {
