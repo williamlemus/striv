53432bb77aefabdada21d849a8a837f1666b6111
William Lemus <william.lemus@gmail.com>
2017-09-03 23:33:00 -0400

    Create event that will handle mouseover on comment

--- frontend/components/comment/comments.jsx
+++ frontend/components/comment/comments.jsx
@@ -3,12 +3,22 @@ import {Link} from 'react-router-dom';
 
 class CommentsShow extends React.Component{
 
+  constructor(props){
+    super(props);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+  }
+
+  handleMouseEnter(e){
+    // Delete workout button will be made to appear here
+  }
+
+
   render() {
     if(this.props.comment_ids.length > 0){
       const comment_ids = this.props.comment_ids;
       const comments = this.props.comments;
       return(
-          <ul className='comments-container'>
+          <ul className='comments-container' onMouseEnter={this.handleMouseEnter}>
             {
               comment_ids.map((id) =>{
                 return(
