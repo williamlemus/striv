64e18a17cd6515f31d201fece01dcb760ea3860a
William Lemus <william.lemus@gmail.com>
2017-09-26 18:38:53 -0400

    Improve test coverage for user associations. Add test for ::generate_session_token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   describe 'Validations' do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
@@ -8,9 +9,16 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:session_token) }
   end
+
   describe 'Associations' do
-    it { should have_many(:workouts) }
-    it { should have_many(:routes) }
-    it { should have_many(:comments) }
+    it { should have_many(:workouts).dependent(:destroy) }
+    it { should have_many(:routes).dependent(:destroy) }
+    it { should have_many(:comments).dependent(:destroy) }
+  end
+
+  describe '::generate_session_token' do
+    it 'should generate session token' do
+      expect(User.generate_session_token).not_to be_nil
+    end
   end
 end
